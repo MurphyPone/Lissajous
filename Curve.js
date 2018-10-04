cca3079dff5bd0f5b4ee6a09f2cd3c0a583a6586
Peter Murphy <petermurphy@Peters-MacBook-Pro-2.local>
2018-10-03 22:18:51 -0400

    init

---
+++ Curve.js
@@ -0,0 +1,29 @@
+class Curve {
+  constructor() {
+    this.path = []; //holds vectors
+    this.current = createVector(); //Current vector which is used to update path
+  }
+
+  addPoint() {
+    this.path.push(this.current);
+  }
+
+  setX(x) { this.current.x = x; }
+  setY(y) { this.current.y = y; }
+
+  show(j, i) {
+    var c = 360 / (i+j);
+    stroke(c, 255, 255); //TODO be thoughtful about color
+    strokeWeight(1);
+    noFill();
+
+    beginShape()
+    for(var v of this.path) { vertex(v.x, v.y); }
+    endShape();
+
+    strokeWeight(5);
+    stroke(255);
+    point(this.current.x, this.current.y);
+    this.current = createVector();
+  }
+}
---
+++ sketch.js
@@ -0,0 +1,118 @@
+var angle = 0;
+var w; // width of the circle should be a ratio to screen
+var rows;
+var cols;
+var cx, cy; //center x,y
+var x, y; //polar coords
+var d, d; //diameter, radius
+var off; //offset
+var curves; //2D array of curves
+var c;
+
+function setup() {
+  createCanvas(windowHeight, windowHeight );
+
+  colorMode(HSB);
+  w = width / 12;
+  cols = floor(width / w) - 1;
+  rows = floor(height / w) - 1;
+  d = w - .2 * w; //diameter
+  r = d / 2;
+  // depricate
+  curves = [];
+  for (var j = 0; j < rows; j++) {
+    curves[j] = [];
+
+    for (var i = 0; i < cols; i++) {
+      curves[j][i] = new Curve();
+    }
+  }
+}
+
+function draw() {
+  background(20);
+  //configure draw settings
+  stroke(255);
+  noFill();
+
+  drawCols();
+  drawRows();
+  drawPaths();
+
+  //increase da angle, yo
+  if (angle <= TWO_PI) {
+    angle += .01;
+  }
+
+}
+
+
+//Helper methods
+function drawCols() {
+  //Draw cols
+  for (var i = 0; i < cols; i++) {
+    //get color
+    c = 360 / (i+1);
+
+    strokeWeight(1);
+    stroke(c, 255 ,255);
+    cx = w + i * w + w / 2;
+    cy = w / 2;
+    ellipse(cx, cy, d, d); //big circle
+
+    //draw points
+    strokeWeight(8);
+    x = r * cos(angle * (i + 1) - PI / 2); //polar conversion
+    y = r * sin(angle * (i + 1) - PI / 2);
+    stroke(255, 150);
+    point(cx + x, cy + y);
+
+    //draw tracer lines //TODO cMOD HERE
+    strokeWeight(1);
+    line(cx + x, cy + y, cx + x, height);
+
+    //prep curves
+    for (var j = 0; j < rows; j++) {
+      stroke(i%256, 255, 255)
+      curves[j][i].setX(cx + x);
+    }
+
+  }
+}
+
+function drawRows() {
+  for(var j = 0; j < rows; j++) {
+    c = 360 / (j+1);
+
+    strokeWeight(1);
+    stroke(c, 255, 255);
+    cx = w / 2;
+    cy = w + j * w + w / 2;
+    ellipse(cx, cy, d, d); //big circle
+
+    //draw points
+    strokeWeight(8);
+    x = r * cos(angle * (j + 1) - HALF_PI); //polar conversion
+    y = r * sin(angle * (j + 1) - HALF_PI);
+    stroke(255, 150);
+    point(cx + x, cy + y);
+
+    //draw tracer lines //TODO cMOD HERE
+    strokeWeight(1);
+    line(cx + x, cy + y, width, cy + y);
+
+    //Draw curves
+    for(var i = 0; i < cols; i++) {
+      curves[j][i].setY(cy + y);
+    }
+  }
+}
+
+function drawPaths() {
+  for (var j = 0; j < rows; j++) {
+    for (var i = 0; i < cols; i++) {
+      curves[j][i].addPoint();
+      curves[j][i].show(j, i);
+    }
+  }
+}
